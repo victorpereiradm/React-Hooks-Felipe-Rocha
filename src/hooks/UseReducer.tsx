b0fe9f353138f17cfd224fe18999c8cb0500d223
Victor Pereira <victorpereirademelo18@gmail.com>
2025-08-08 14:06:50 -0300

    Refactor UseReducer hook to manage task list with add and toggle functionality

--- src/hooks/UseReducer.tsx
+++ src/hooks/UseReducer.tsx
@@ -1,28 +1,45 @@
-import { useReducer } from 'react';
+import { useReducer, useState } from 'react';
 
-const reducer = (state: { count: number }, action: { type: string }): { count: number } => {
-  switch(action.type) {
-    case 'increment':
-      return { count: state.count + 1 };
-    case 'decrement':
-      return { count: state.count - 1 };
-    default:
-      return state;
-  }
+type ActionType = 'add-task' | 'toggle-task';
+
+const reducer = (state: { tasks: { name: string, isCompleted: boolean }[] }, action: { type: ActionType, payload: string | number }): { tasks: { name: string, isCompleted: boolean }[] } => {
+  if (action.payload === '') return state;
+
+  const dictionaryActions = {
+    'add-task': {
+      ...state,
+      tasks: [...state.tasks, { name: action.payload, isCompleted: false }]
+    },
+    'toggle-task': {
+      ...state,
+      tasks: state.tasks.map((task, index) => index === action.payload ? { ...task, isCompleted: !task.isCompleted } : task),
+    }
+  } as Record<ActionType, { tasks: { name: string, isCompleted: boolean }[] }>;
+
+  const actionResult = dictionaryActions[action.type];
+
+  return actionResult || state;
 };
 
 const UseReducer = () => {
-  const [state, dispatch] = useReducer(reducer, { count: 0 });
+  const [inputValue, setInputValue] = useState('');
+  const [state, dispatch] = useReducer(reducer, { tasks: [] });
+
+  const completedTasksCount = state.tasks.filter(task => task.isCompleted).length;
 
   return (
     <div>
-      <p>{state.count}</p>
+        <input type="text" value={inputValue} onChange={(e) => setInputValue(e.target.value) } placeholder="Adicione uma tarefa" />
         <button onClick={() => {
-            dispatch({ type: 'increment' })
-        }}>Increment</button>
-        <button onClick={() => {
-            dispatch({ type: 'decrement' })
-        }}>Decrement</button>
+          dispatch({ type: 'add-task', payload: inputValue });
+          setInputValue('');
+        }}>Adicionar</button>
+
+        <p>Tarefas concluídas: {completedTasksCount}</p>
+
+        {state.tasks.map((task, index) => (
+            <p key={index} onClick={() => dispatch({ type: 'toggle-task', payload: index })} style={{ color: task.isCompleted ? 'green' : 'white', cursor: 'pointer' }}>{task.name}</p>
+        ))}
     </div>
   );
 };
